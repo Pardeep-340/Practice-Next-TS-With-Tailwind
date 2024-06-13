b784271b80a6406619de92cc5338463563ec98d8
Pardeep-340 <pardeepjangra514@gmail.com>
2024-06-13 09:28:19 +0530

    /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ const page = () => {
   return (
     <div className='relative h-screen'>
       <div className='absolute top-1/2 start-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-4'>
-        <Link href="/todo" className='bg-gray-500 px-5 py-2 rounded-lg inline-block '>Todo</Link>
-        <Link href="/select" className='bg-gray-500 px-5 py-2 rounded-lg inline-block'>Select</Link>
+        <Link href="/todo" className='bg-gray-500 px-5 py-2 rounded-lg inline-block text-white'>Todo</Link>
+        <Link href="/select" className='bg-gray-500 px-5 py-2 rounded-lg inline-block text-white'>Select</Link>
       </div>
     </div>
   )
